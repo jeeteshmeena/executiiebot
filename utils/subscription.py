ca52095a76d724cd6cbe7868e13775e9753c0565
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 18:14:50 +0530

    Update subscription.py

--- utils/subscription.py
+++ utils/subscription.py
@@ -1,7 +1,10 @@
+async def check_sub(client, user, channels):
 
-async def check_subscription(client, user_id, channels):
     for ch in channels:
-        member = await client.get_chat_member(ch, user_id)
+
+        member = await client.get_chat_member(ch, user)
+
         if member.status == "left":
             return False
+
     return True
