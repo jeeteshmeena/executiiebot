01e557b8770f24ad8eba69205a9a487ddb12f89e
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 18:15:18 +0530

    Update stories.py

--- handlers/stories.py
+++ handlers/stories.py
@@ -1,16 +1,20 @@
-
 from pyrogram import filters
 from database.mongo import stories
 
 def register_stories(bot):
+
     @bot.on_message(filters.command("stories"))
-    async def list_stories(client, message):
-        data = stories.find()
+
+    async def stories_list(client, message):
+
         text = "📚 Available Stories\n\n"
+
         i = 1
-        async for s in data:
+
+        async for s in stories.find():
+
             text += f"{i}. {s['story_name']}\n"
+
             i += 1
-        if i == 1:
-            text += "No stories found."
+
         await message.reply_text(text)
