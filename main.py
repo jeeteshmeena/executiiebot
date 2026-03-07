21acd597d5920cea946975da4bd63936ff1b1c5a
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 22:27:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
 from pyrogram import Client
 from flask import Flask
-import os
 
 from handlers.start import register_start
 from handlers.help import register_help
@@ -11,7 +11,19 @@ from handlers.stories import register_stories
 from handlers.range import register_range
 from handlers.story_select import register_story_select
 
-app = Client("executiiebot")
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+
+app = Client(
+    "executiiebot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
 
 register_start(app)
 register_help(app)
@@ -22,7 +34,9 @@ register_stories(app)
 register_range(app)
 register_story_select(app)
 
+
 print("ExecutiieBot started")
+
 
 web = Flask(__name__)
 
@@ -30,7 +44,9 @@ web = Flask(__name__)
 def home():
     return "ExecutiieBot is running"
 
+
 if __name__ == "__main__":
     app.start()
+
     port = int(os.environ.get("PORT", 10000))
     web.run(host="0.0.0.0", port=port)
