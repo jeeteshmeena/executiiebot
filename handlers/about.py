786894b7be5974937824aa6a8499879e9953f902
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 22:08:36 +0530

    Update about.py

--- handlers/about.py
+++ handlers/about.py
@@ -1,4 +1,7 @@
-from pyrogram import Client, filters
+from pyrogram import filters
+import asyncio
+from utils.cleanup import auto_delete
+
 
 ABOUT_TEXT = """
 🎭 <b>About</b>
@@ -15,11 +18,28 @@ ABOUT_TEXT = """
 <i>Version: V1.0</i>
 """
 
-@Client.on_message(filters.command("about"))
-async def about_handler(client, message):
 
-    await message.reply_text(
-        ABOUT_TEXT,
-        parse_mode="html",
-        disable_web_page_preview=True
-    )
+def register_about(app):
+
+    @app.on_message(filters.command("about"))
+    async def about_handler(client, message):
+
+        msg = await message.reply_text(
+            ABOUT_TEXT,
+            parse_mode="html",
+            disable_web_page_preview=True
+        )
+
+        try:
+            await message.delete()
+        except:
+            pass
+
+        asyncio.create_task(
+            auto_delete(
+                client,
+                message.chat.id,
+                [msg.id],
+                600
+            )
+        )
