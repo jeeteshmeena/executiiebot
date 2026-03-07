d1ad1973d9952b3586187d93341c11ce7b8959ea
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 17:59:21 +0530

    Update buttons.py

--- utils/buttons.py
+++ utils/buttons.py
@@ -1,11 +1,19 @@
-
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def range_buttons(total):
+def create_range_buttons(total):
+
     buttons = []
+
     start = 1
+
     while start <= total:
-        end = min(start+99, total)
-        buttons.append([InlineKeyboardButton(f"{start}-{end}", callback_data=f"{start}-{end}")])
-        start = end+1
+
+        end = min(start + 99, total)
+
+        buttons.append(
+            [InlineKeyboardButton(f"{start}-{end}", callback_data=f"range_{start}_{end}")]
+        )
+
+        start = end + 1
+
     return InlineKeyboardMarkup(buttons)
