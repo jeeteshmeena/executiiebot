bb0dccde63b408c58f51a84e142b4ce2d34a7626
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 22:07:02 +0530

    Update range.py

--- handlers/range.py
+++ handlers/range.py
@@ -1,39 +1,36 @@
-from pyrogram import Client
-from pyrogram.types import CallbackQuery
 from database.mongo import episodes
 from utils.cleanup import auto_delete
 import asyncio
 
-async def send_range(client: Client, query: CallbackQuery):
-
-    data = query.data.split("|")
-
-    story = data[1]
-    start = int(data[2])
-    end = int(data[3])
-
-    eps = episodes.find({
-        "story": story,
-        "episode": {"$gte": start, "$lte": end}
-    }).sort("episode", 1)
-
-    sent = []
-
-    async for ep in eps:
-
-        m = await query.message.reply_audio(ep["file_id"])
-        sent.append(m.id)
-
-    asyncio.create_task(auto_delete(
-        client,
-        query.message.chat.id,
-        sent,
-        21600
-    ))
-
-def register_range(app: Client):
+def register_range(app):
 
     @app.on_callback_query()
-    async def range_handler(client, query: CallbackQuery):
-        if query.data.startswith("range"):
-            await send_range(client, query)
+    async def range_handler(client, query):
+
+        if not query.data.startswith("range"):
+            return
+
+        data = query.data.split("|")
+
+        story = data[1]
+        start = int(data[2])
+        end = int(data[3])
+
+        eps = episodes.find({
+            "story": story,
+            "episode": {"$gte": start, "$lte": end}
+        }).sort("episode", 1)
+
+        sent = []
+
+        async for ep in eps:
+
+            m = await query.message.reply_audio(ep["file_id"])
+            sent.append(m.id)
+
+        asyncio.create_task(auto_delete(
+            client,
+            query.message.chat.id,
+            sent,
+            21600
+        ))
