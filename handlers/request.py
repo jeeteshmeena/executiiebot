7873b49e4786ab7c313d0460b6fbdc38b73cbd5f
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 21:12:27 +0530

    Update request.py

--- handlers/request.py
+++ handlers/request.py
@@ -1,13 +1,33 @@
+from pyrogram import Client, filters
 
-from pyrogram import filters
-from database.mongo import requests
+REQUEST_OK = """
+✅ <b>Your request has been submitted!</b>
+"""
 
-def register_request(bot):
-    @bot.on_message(filters.command("request"))
-    async def request_story(client, message):
-        text = message.text.replace("/request","").strip()
-        if not text:
-            await message.reply_text("Please provide story name.")
-            return
-        await requests.insert_one({"story": text})
-        await message.reply_text("✅ Request submitted.")
+REQUEST_NEED = """
+🎬 <b>Please provide the name of story</b>
+
+📝 <b>Examples:</b>
+<code>/request Yakshini</code>
+<code>/request Saaya A Cursed Love Story</code>
+"""
+
+@Client.on_message(filters.command("request"))
+async def request_handler(client, message):
+
+    text = message.text.split(maxsplit=1)
+
+    if len(text) == 1:
+
+        await message.reply_text(
+            REQUEST_NEED,
+            parse_mode="html"
+        )
+        return
+
+    story = text[1]
+
+    await message.reply_text(
+        REQUEST_OK,
+        parse_mode="html"
+    )
