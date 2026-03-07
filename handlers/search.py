792a30af4e529cae7f6de5e7d9594b79bfe1f9de
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 19:14:32 +0530

    Update search.py

--- handlers/search.py
+++ handlers/search.py
@@ -4,15 +4,14 @@ from utils.buttons import create_range_buttons
 
 def register_search(bot):
 
-    @bot.on_message(filters.text)
-
+    @bot.on_message(filters.text & ~filters.command(["start","help","stories","latest","request"]))
     async def search(client, message):
 
         query = message.text.lower()
 
-        story = await stories.find_one(
-            {"story_name": {"$regex": query, "$options": "i"}}
-        )
+        story = await stories.find_one({
+            "story_name": {"$regex": query, "$options": "i"}
+        })
 
         if not story:
             return
