707150ca39474c0fd152b0941078e39aeaf79fdf
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 21:45:37 +0530

    Update request.py

--- handlers/request.py
+++ handlers/request.py
@@ -1,10 +1,14 @@
 from pyrogram import Client, filters
+from utils.cleanup import auto_delete
+import asyncio
 
-REQUEST_OK = """
+REQUEST_CHANNEL = -1003714374498
+
+OK = """
 ✅ <b>Your request has been submitted!</b>
 """
 
-REQUEST_NEED = """
+NEED = """
 🎬 <b>Please provide the name of story</b>
 
 📝 <b>Examples:</b>
@@ -15,19 +19,39 @@ REQUEST_NEED = """
 @Client.on_message(filters.command("request"))
 async def request_handler(client, message):
 
+    cmd = message.id
+
     text = message.text.split(maxsplit=1)
 
     if len(text) == 1:
 
-        await message.reply_text(
-            REQUEST_NEED,
-            parse_mode="html"
-        )
+        m = await message.reply_text(NEED, parse_mode="html")
+
+        await message.delete()
+
+        asyncio.create_task(auto_delete(
+            client,
+            message.chat.id,
+            [m.id],
+            600
+        ))
+
         return
 
     story = text[1]
 
-    await message.reply_text(
-        REQUEST_OK,
-        parse_mode="html"
+    await client.send_message(
+        REQUEST_CHANNEL,
+        f"📩 Request\nUser: {message.from_user.mention}\nStory: {story}"
     )
+
+    m = await message.reply_text(OK, parse_mode="html")
+
+    await message.delete()
+
+    asyncio.create_task(auto_delete(
+        client,
+        message.chat.id,
+        [m.id],
+        600
+    ))
