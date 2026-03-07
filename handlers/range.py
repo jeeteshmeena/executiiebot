b721589da4516176393f783bb003b50db467317f
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 18:13:33 +0530

    Create range.py

---
+++ handlers/range.py
@@ -0,0 +1,21 @@
+from pyrogram import filters
+from database.mongo import episodes
+
+def register_range(bot):
+
+    @bot.on_callback_query(filters.regex("range_"))
+
+    async def send_range(client, query):
+
+        data = query.data.split("_")
+
+        start = int(data[1])
+        end = int(data[2])
+
+        eps = episodes.find(
+            {"episode_start": {"$lte": end}, "episode_end": {"$gte": start}}
+        )
+
+        async for ep in eps:
+
+            await query.message.reply_document(ep["file_id"])
