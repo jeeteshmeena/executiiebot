1cd8bb5d66f6ae224f8b732074f459ad189e1fca
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 21:07:58 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,21 @@
+from pyrogram import Client, filters
 
-from pyrogram import filters
+@Client.on_message(filters.command("start"))
+async def start_handler(client, message):
 
-def register_start(bot):
-    @bot.on_message(filters.command("start"))
-    async def start(client, message):
-        await message.reply_text(
-            "✨ Welcome to ExecutiieBot!\n\n"
-            "Use /stories to see available stories."
-        )
+    text = """
+✨ <b>Welcome, {}</b>
+
+<b>𝗘𝘅𝗲𝗰𝘂𝘁𝗶𝗶𝗕𝗼𝘁 — Audio Story Search Bot 🤖</b>
+
+<b>Commands:</b> /latest • /help • /request • /about • /stories
+
+<b>Disclaimer 📌</b>
+<i>We only index Telegram files. We do not host content.</i>
+
+<u>Send your query to begin!</u>
+
+<a href="https://t.me/MeJeetX">📢 Channel</a> | <a href="https://t.me/+HvKfFsPziO42OTNl">💬 Group</a>
+""".format(message.from_user.first_name)
+
+    await message.reply_text(text)
